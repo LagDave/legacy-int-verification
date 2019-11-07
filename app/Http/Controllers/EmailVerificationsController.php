cf2f0eaeeb177602198b400ffc99cef3ce63161a
rdavephp <rdavephp@gmail.com>
2019-11-06 16:36:04 -0800

    'Updates'

--- app/Http/Controllers/EmailVerificationsController.php
+++ app/Http/Controllers/EmailVerificationsController.php
@@ -7,8 +7,10 @@ use Illuminate\Support\Facades\Mail;
 
 class EmailVerificationsController extends Controller
 {
+    public $email;
     public function index($email){
 
+        $this->email = $email;
         $random_key = mt_rand(150000, 990000);
 
         $data = [
@@ -17,7 +19,7 @@ class EmailVerificationsController extends Controller
         ];
 
         Mail::send('mail.test', $data, function($message){
-            $message->to($email, '')->subject("Verify your Legacy-Interactive Account!");
+            $message->to($this->email, '')->subject("Verify your Legacy-Interactive Account!");
         });
 
         return view('verification.index', compact('email','random_key'));
